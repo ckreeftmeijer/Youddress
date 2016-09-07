17a53773d05959ec9cbc00805157295c7b0b68d0
ckreeftmeijer <ckreeftmeijer@gmail.com>
2016-09-07 15:24:43 +0200

    data from facebook api

--- index.ios.js
+++ index.ios.js
@@ -14,7 +14,11 @@ const {
   LoginButton,
   GraphRequest,
   GraphRequestManager,
+  AccessToken,
 } = FBSDK;
+
+
+
 
 class Youddress extends Component {
   constructor(props) {
@@ -26,14 +30,73 @@ class Youddress extends Component {
 
     this.state = {
       shareLinkContent: shareLinkContent,
+      isLoading: false,
+      message: '',
     };
   }
+//
+//   _handleResponse(response) {
+//   this.setState({ isLoading: false , message: '' });
+//   if (response.application_response_code.substr(0, 1) === '1') {
+//     console.log('Properties found: ' + response.listings.length);
+//   } else {
+//     this.setState({ message: 'Location not recognized; please try again.'});
+//   }
+// }
+//
+//   componentDidMount(){
+//     AccessToken.getCurrentAccessToken().then(
+//       (data) => {
+//         let accessToken = data.accessToken
+//         console.log(accessToken.toString())
+//
+//         fetch('https://graph.facebook.com/me?access_token=' + accessToken.toString())
+//         .then(response => response.json())
+//         .then(json => this._handleResponse(json.response))
+//         .catch(error =>
+//            this.setState({
+//             isLoading: false,
+//             message: 'Something bad happened ' + error
+//          }));
+//       }
+//     )
+//
+//   }
 
+_responseInfoCallback(error: ?Object, result: ?Object) {
+  if (error) {
+    alert('Error fetching data: ' + error.toString());
+  } else {
+    console.log(result);
+    // this.setState({ message: 'Location not recognized; please try again.'});
+  }
+}
 
   render() {
+
+    AccessToken.getCurrentAccessToken().then(
+      (data) => {
+        let accessToken = data.accessToken
+        console.log(accessToken.toString())
+
+    const infoRequest = new GraphRequest(
+    '/me',{
+                  accessToken: accessToken.toString(),
+                  parameters: {
+                    fields: {
+                      string: 'email,name,first_name,middle_name,last_name,friends{name,picture}'
+                    }
+                  }
+                },
+    this._responseInfoCallback,
+  );
+
+  new GraphRequestManager().addRequest(infoRequest).start();
+})
     return (
       <View style={styles.container}>
         <Login />
+        <Text>{this.state.message}</Text>
       </View>
     );
   }
