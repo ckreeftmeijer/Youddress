4074e986b6283ad3ec2b8f08b0a5ffb075e643a1
ckreeftmeijer <ckreeftmeijer@gmail.com>
2016-09-12 11:08:36 +0200

    Add initial route - LoginPage

--- index.ios.js
+++ index.ios.js
@@ -1,107 +1,37 @@
 
 import React, { Component } from 'react';
-import Login from "./Login.js"
-import Friends from "./components/Friends.js"
+import LoginPageNav from "./src/pages/LoginPage.ios"
 import {
   AppRegistry,
   StyleSheet,
   Text,
   TouchableHighlight,
-  View
+  View,
+  NavigatorIOS,
 } from 'react-native';
-
-const FBSDK = require('react-native-fbsdk');
-const {
-  LoginButton,
-  GraphRequest,
-  GraphRequestManager,
-  AccessToken,
-} = FBSDK;
-
 
 
 
 class Youddress extends Component {
-  constructor(props) {
-    super(props);
-    const shareLinkContent = {
-      contentType: 'link',
-      contentUrl: "https://www.facebook.com/",
-    };
 
-    this.state = {
-      shareLinkContent: shareLinkContent,
-      friends: [],
-    };
-  }
-
-  componentWillMount(){
-    AccessToken.getCurrentAccessToken().then(
-      (data) => {
-        let accessToken = data.accessToken
-        // console.log(accessToken.toString())
-
-    const infoRequest = new GraphRequest(
-    '/me',{
-                  accessToken: accessToken.toString(),
-                  parameters: {
-                    fields: {
-                      string: 'email,name,first_name,middle_name,last_name,friends{name,picture}'
-                    }
-                  }
-                },
-    this._responseInfoCallback.bind(this),
-  );
-
-  new GraphRequestManager().addRequest(infoRequest).start();
-})
-  }
-
-
-_responseInfoCallback(error: ?Object, result: ?Object) {
-  if (error) {
-    alert('Error fetching data: ' + error.toString());
-  } else {
-    // console.log(result);
-    // console.log(result.friends);
-    // console.log(result.friends.data[0]);
-    // this.setState( { graphData: result } )
-      this.setState( {
-         fbID: result.id,
-         fullName: `${result.first_name} ${result.last_name}`,
-         firstName: result.first_name,
-         lastName: result.last_name,
-         friends: result.friends,
-       } )
-
-       console.log(result.friends.data[0].name)
-    }
-}
 
   render() {
 
     return (
-      <View style={styles.container}>
-        <Login />
-        {/* <Text>{ this.state.fullName } </Text> */}
-          {/* <Text>{ this.state.friendz } </Text> */}
-        <Friends friends={this.state.friends}/>
-      </View>
-    );
+      <NavigatorIOS
+             style={styles.container}
+             initialRoute={{
+               title: 'Login Page',
+               component: LoginPageNav,
+             }}/>
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  shareText: {
-    fontSize: 20,
-    margin: 10,
-  }
 });
 
 
